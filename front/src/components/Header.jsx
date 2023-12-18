084621de62209616b0b3b1b60a17862b350d3792
Antônio Cruz <antoniocruzgomes880@gmail.com>
2023-12-18 14:37:43 -0300

    feat: front praticamente terminado, feito os componentes pensando no back

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -11,7 +11,6 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
 import { useUserContext } from "./ContextUser";
 
 const pages = ["Products", "Pricing", "Blog"];
